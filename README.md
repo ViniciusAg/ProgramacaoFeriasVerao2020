d52fe0dd4c4ee5c848026bf293fc5deb58490fbc
vaiola3 <newillianpereira@gmail.com>
2019-12-21 12:42:35 -0300

    fix incluindo README e arrays associativos

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ProgramacaoFeriasVerao2020
+
+#   jojoPhpProject
+
+estou tocando um projeto pessoal que idealizei a pouco mais de 8 meses
+
+pretendo aprender conceitos de Orientação a Objetos e de Arquitetura MVC utilizando PHP
+
+estou utilizando os seguintes materiais como referência:
+
+    !)https://www.udemy.com/course/php-do-basico-ao-intermediario/learn/lecture/6990286?start=765#overview
+
+    !)https://www.udemy.com/course/laravelcompleto/learn/lecture/15592844?start=0#overview
+
+    !)https://riptutorial.com/php
+
+    !)https://riptutorial.com/laravel
+
+o projeto consiste em uma aplicação web leve e robusta que permita a professores realizarem a chamada em sala de aula utilizando o próprio celular
+
+a principio seria uma tela com os nomes dos alunos e um checkbox que permitiria ao professor marcar os alunos presentes e ao final da chamada submeter para um servidor um formulário com a informação de alunos presentes
+
+com esses dados os coordenadores teriam um melhor controle da frequência dos alunos e da participação desses nas disciplinas
--- index.php
+++ index.php
@@ -14,12 +14,29 @@
     }   ////    #   verifys de occurrency of a string in another
 
     $sTeste = "";
-    // $sTeste .= "1";
-    $sTeste .= "2";
+    // $sTeste .= "1";  ////    simplewest hello world
+    // $sTeste .= "2";  ////    making arrays and showing data from them
+    $sTeste .= "3";  ////    associative arrays ( why not a oldest type of json??? )
+
+    if ( occurOf( $sTeste, "3" ) ){
+        $sArrayAssociativo = [
+            "item1" => "seria",
+            "item2" => "eu",
+            "item3" => "um",
+            "item4" => "objeto",
+            "item5" => "ou",
+            "item6" => "alguma",
+            "item7" => "especie",
+            "item8" => "de ser hibrido?"
+        ];
+
+        echo "array associativo " . $sArrayAssociativo[ "item4" ];
+    }   ////    #   associative arrays ( why not a oldest type of json??? )
 
     if ( occurOf( $sTeste, "2" ) ){
         $sArray = array( "arcas", "fabil", "luccas", "roninho", "burno" );
         $iTamanhoArray = count( $sArray );
+
         $iAux = 0;
 
         echo    "<select id='selectArrays' onchange='iSelectedData = this.value'>";
