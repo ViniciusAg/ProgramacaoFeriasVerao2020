007f6927280784684e4609da66f39da0d6fe605f
vaiola3 <newillianpereira@gmail.com>
2019-12-28 01:56:50 -0300

    setando models

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -4,41 +4,7 @@
 
         function validateLogin( $sLoginUser, $sLoginPass ){
 
-            ////    conecta no dataBase
-
-            $sDbServer = "localhost";
-            $sDbName = "programacaoferiasverao2020";
-            $sDbUser = "root";
-            $sDbPass = "";
-            $oDbLink = new mysqli( $sDbServer, $sDbUser, $sDbPass, $sDbName );
-
-            $oDbLink->set_charset( "utf8" );
-
-            ////    conecta no dataBase final
-
-            ////    valida login
-
-            if ( $sLoginPass ){
-                $sQuery  = "select * from cadastros where ( senha = '" . md5( $sLoginPass ) . "' )";
-
-                $sRes = $oDbLink->query( $sQuery );
-
-                $sEvaluated = $sRes->fetch_assoc();
-
-                if ( $sRes->num_rows ){
-                    if ( $sEvaluated[ "tipo" ] == "X" )
-                        $sController = new AdminController;
-                    if ( $sEvaluated[ "tipo" ] == "A" )
-                        $sController = new AlunoController;
-                    if ( $sEvaluated[ "tipo" ] == "P" )
-                        $sController = new ProfessorController;
-                }else
-                    $sController = new HomeController;
-
-                $sController->index();
-            }
-
-            ////    valida login final
+            
         }
 
         public function start(){
