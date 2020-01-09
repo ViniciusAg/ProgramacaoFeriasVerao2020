19b2cef47b83954b06a96ea9c4eaede054e80353
GuilhermeDaniluski <guilhermedaniluski@gmail.com>
2020-01-08 21:36:46 -0300

    Testing/click-counter: First Commit

---
+++ GuilhermeDaniluski/AutomatedTesting/click-counter/src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Enzyme, {shalow} from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+import App from './App'
+
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+test('renders without error', ()=>{
+
+})
+
+test('renders increment button', () => {
+  
+});
+
+test('renders display counter', () => {
+  
+});
+
+test('counter start at zero', () => {
+  
+});
