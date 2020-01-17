3ef1882d94e15d5678860e05d14c650035e28701
Murilo-ZC <carvalho.zanini@gmail.com>
2020-01-17 16:57:15 -0300

    Utilizando let.

---
+++ cod3r_web_2020/fundamentos/usandoLet.js
@@ -0,0 +1,9 @@
+//Definição de variáveis com let
+let variavelGlobal = 4
+
+{
+    let variavelGlobal = 3
+    console.log("Dentro do bloco:", variavelGlobal)
+}
+
+console.log("Fora do bloco:", variavelGlobal)
